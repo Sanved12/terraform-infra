c04f64e619b3fa6a77fa61682b7b8b070f4216cf
Sanved Sharad Kabade <sanvedkabade101@example.com>
2026-05-13 11:12:13 +0100

    check

--- modules/cicd/cicd.tf
+++ modules/cicd/cicd.tf
@@ -211,7 +211,7 @@ resource "aws_codepipeline" "terraform" {
   }
 
   stage {
-    name = "Source"
+    name = "CodeSource-GitHub"
 
     action {
       name             = "GitHub_Source"
@@ -231,7 +231,7 @@ resource "aws_codepipeline" "terraform" {
   }
 
   stage {
-    name = "Terraform_Apply"
+    name = "CodeBuild-Terraform"
 
     action {
       name             = "Terraform_Build"
